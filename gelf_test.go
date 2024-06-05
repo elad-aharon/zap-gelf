81ef4923ebc8a33ab09533c7b820bc3e6b4b31cf
elad-aharon <elad.aharon@similarweb.com>
2024-06-05 15:39:18 +0300

    fix test package

--- gelf_test.go
+++ gelf_test.go
@@ -6,7 +6,7 @@ import (
 	"os"
 	"testing"
 
-	gelf "github.com/snovichkov/zap-gelf"
+	gelf "github.com/elad-aharon/zap-gelf"
 	"github.com/stretchr/testify/assert"
 	"go.uber.org/zap"
 	"go.uber.org/zap/zapcore"
